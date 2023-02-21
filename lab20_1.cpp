a3da5c18ee7e5df596bbde954cbb37e6c8cfe5fa
Natan <Th3than@gmail.com>
2023-02-21 15:44:30 +0700

    lab20-1

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,14 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect x,Rect y){
+	double len = 0 , hei = 0;
+	len = min(x.x+x.w,y.x+y.w)-max(x.x,y.x);
+	hei = min(x.y,y.y)-max(x.y-x.h,y.y-y.h);
+	if (hei < 0 || len < 0)
+	{
+		return 0;
+	}
+	
+	return len*hei;
 }
